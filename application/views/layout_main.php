30c5bd0e77a758ac5460a6a5970bd21fc0af6148
kylecarlson <krcz85@mail.missouri.edu>
2015-01-28 23:46:23 -0600

    Jumbotron setup

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -10,27 +10,23 @@
 
 </head>
 <body>
-	
 
-	
-	<div class="container" >
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Project name</a>
-				</div>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Project name</a>
 			</div>
-		</nav>	
-		
-		<?php echo $content_for_layout ?>
-	</div>
+		</div>
+	</nav>	
 	
+	<?php echo $content_for_layout ?>
+
 	<div> Footer </div>
 	
 	<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,15 +1,18 @@
-<div class="hero-unit">
-	<h1>Welcome to CodeIgniter!</h1>
+<div class="jumbotron">
+	<div class="container">
+		<h1>Welcome to CodeIgniter!</h1>
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 	
-	<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-	<p>If you would like to edit this page you'll find it located at:</p>
-	<code>application/views/welcome_message.php</code>
-
-	<p>The corresponding controller for this page is found at:</p>
-	<code>application/controllers/welcome.php</code>
-
-	<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-
-	<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+	
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/welcome.php</code>
+	
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	
+		<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+		
+		<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+	</div>
 </div>
