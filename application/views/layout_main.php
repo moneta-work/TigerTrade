6c38d7e0e848979396801ff9c3b9dbf1a24cdfea
kylecarlson <krcz85@mail.missouri.edu>
2015-01-28 23:33:08 -0600

    whoops

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -5,7 +5,7 @@
 	<!-- <link rel="stylesheet" href="/css/main.css" type="text/css" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     
-    <<link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
 
 </head>
