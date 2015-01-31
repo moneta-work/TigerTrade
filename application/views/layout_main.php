96acd39e81a02be8397f47f75936ebf07f102900
kylecarlson <krcz85@mail.missouri.edu>
2015-01-30 22:42:15 -0600

    footer

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -71,7 +71,7 @@
 					<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 				</div>
 				<div class="col-lg-4">
-					<p class="center"><a href="#">Back to top</a></p>
+					<p class="pull-center"><a href="#">Back to top</a></p>
 				</div>
 				<div class="col-lg-4">
 					<p class="pull-right">&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
