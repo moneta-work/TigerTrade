345919634f0c03eeec2e93d197001ee6bd1b6046
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 03:29:40 -0600

    naming

--- application/controllers/ad.php
+++ application/controllers/ad.php
@@ -7,7 +7,7 @@ class Ad extends CI_Controller
 		parent::__construct();
 
 		$this->load->helper('url');
-		$this->load->library('breadcrumbcomponent');
+		$this->load->library('breadcrumb');
 	}
 
 	function index()
