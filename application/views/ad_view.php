f48b273f82c373f56224cc9c827f6b2c53ee6e06
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 02:40:33 -0600

    sdf

--- application/views/ad_view.php
+++ application/views/ad_view.php
@@ -1,5 +1,8 @@
 <div class="container padding-top-20">
 	<h1>Ad Index</h1>
+	
+	<p><button class="btn btn-default" onclick="goBack()">Back</button></p>
+	
 	<p>Will this be the controller for single ad views?</p>
 	<a href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a>
 </div>
