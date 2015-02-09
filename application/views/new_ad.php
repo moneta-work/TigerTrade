024cfd5cadb1cb836bc2cdcaee0e0786facf3b22
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 01:52:57 -0600

    css

--- application/views/new_ad.php
+++ application/views/new_ad.php
@@ -6,13 +6,13 @@
 		<div class="form-group">
 			<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 			<div class="col-sm-10">
-				<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+				<input type="email" class="form-control" id="" placeholder="Email">
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
 			<div class="col-sm-10">
-				<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+				<input type="password" class="form-control" id="" placeholder="Password">
 			</div>
 		</div>
 		<div class="form-group">
