2b0a53ae1793ed2c04a14aefab0b20f2d58aafcf
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 02:07:37 -0600

    form

--- application/views/new_ad.php
+++ application/views/new_ad.php
@@ -4,15 +4,15 @@
 	
 	<form class="form-horizontal" id="ad-form">
 		<div class="form-group">
-			<label for="title" class="col-sm-2 control-label">Title</label>
+			<label for="title" class="col-sm-2 control-label label-20">Title</label>
 			<div class="col-sm-10">
 				<input type="text" class="form-control" id="title" placeholder="...">
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="price" class="col-sm-2 control-label">price</label>
+			<label for="price" class="col-sm-2 control-label label-20">price</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="price" placeholder="$0.00">
+				<textbox type="text" class="form-control" id="price" placeholder="$0.00">
 			</div>
 		</div>
 		<div class="form-group">
@@ -39,7 +39,7 @@
 	
 }
 
-label {
+.label-20 {
 	font-size: 20px;
 }
 
