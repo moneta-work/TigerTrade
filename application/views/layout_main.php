1e91f2e617e64d49516ed43cba9d5a411df5321b
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 16:24:50 -0600

    fix>

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title><?php if($title) { echo $title; } else { echo "TigerTrade"; } ?></title>
+	<title><?php if(!$title) { echo "TigerTrade"; } else { echo $title; } ?></title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge;text/html;charset=utf-8">
 	<!-- <link rel="stylesheet" href="/css/main.css" type="text/css" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
