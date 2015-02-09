4cff74e688dd7897e3e343bb42c0055ce749307d
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 01:52:31 -0600

    css

--- application/views/new_ad.php
+++ application/views/new_ad.php
@@ -1,27 +1,34 @@
 <div class="container">
 
 	<h1>New Ad Form</h1>
-
+	
 	<form class="form-horizontal">
 		<div class="form-group">
-			<label for="exampleInputEmail1">Title</label>
-			<input type="email" class="form-control" id="" placeholder="Enter Listing Title">
+			<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+			</div>
 		</div>
 		<div class="form-group">
-			<label for="exampleInputPassword1">Password</label>
-			<input type="password" class="form-control" id="" placeholder="Password">
+			<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+			<div class="col-sm-10">
+				<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+			</div>
 		</div>
 		<div class="form-group">
-			<label for="exampleInputFile">File input</label>
-			<input type="file" id="">
-			<p class="help-block">Example block-level help text here.</p>
+			<div class="col-sm-offset-2 col-sm-10">
+				<div class="checkbox">
+					<label>
+						<input type="checkbox"> Remember me
+					</label>
+				</div>
+			</div>
 		</div>
-		<div class="checkbox">
-			<label>
-				<input type="checkbox"> Check me out
-			</label>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default">Sign in</button>
+			</div>
 		</div>
-		<button type="submit" class="btn btn-default">Submit</button>
 	</form>
 
 </div>
