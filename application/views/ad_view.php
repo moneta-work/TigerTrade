8d70e4944888feaefe88fff03d48ee0a56d0779d
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 03:03:15 -0600

    css

--- application/views/ad_view.php
+++ application/views/ad_view.php
@@ -11,3 +11,11 @@
 	<p>Will this be the controller for single ad views?</p>
 	<a href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a>
 </div>
+
+<style>
+
+h1 {
+	padding-left: 10px;
+}
+
+</style>
