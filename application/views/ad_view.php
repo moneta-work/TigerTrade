cb6154a5d854a8eebc2663435af6939f1c0c5113
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 01:02:02 -0600

    padding fixes

--- application/views/ad_view.php
+++ application/views/ad_view.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container padding-top-20">
 	<h1>Ad Index</h1>
 	<p>Will this be the controller for single ad views?</p>
 </div>
--- application/views/user_home.php
+++ application/views/user_home.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container padding-top-20">
 	<h1>User Homepage</h1>
 	<p>This will be the homepage for logged in users.</p>
 </div>
