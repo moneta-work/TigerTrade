1acdbf7cd92048a7692dffd4973ac7c3f466855e
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 02:33:40 -0600

    link to ad

--- application/views/ad_view.php
+++ application/views/ad_view.php
@@ -1,4 +1,5 @@
 <div class="container padding-top-20">
 	<h1>Ad Index</h1>
 	<p>Will this be the controller for single ad views?</p>
+	<a href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a>
 </div>
