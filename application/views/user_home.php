4cb2bcd3329ac80cad8c489fbe80a03a347c5c37
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 03:24:21 -0600

    css

--- application/views/user_home.php
+++ application/views/user_home.php
@@ -1,4 +1,12 @@
 <div class="container padding-top-20">
-	<h1>User Homepage</h1>
+	<div class="row">
+		<div class="col-xs-1">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-11">
+			<h1 class="custom-h1">User Homepage</h1>
+		</div>
+	</div>
+	
 	<p>This will be the homepage for logged in users.</p>
 </div>
