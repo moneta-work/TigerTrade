4f05cc4cba442e7b1f37ca5b1eae3fdea62bca74
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 02:31:56 -0600

    title

--- application/controllers/custom_404.php
+++ application/controllers/custom_404.php
@@ -8,7 +8,7 @@ class Custom_404 extends CI_Controller
 
     public function index() 
     { 
-        $this->output->set_status_header('404'); 
+        $this->output->set_status_header('404 Not Found'); 
         $this->layout->view('custom_404');
     } 
 } 
