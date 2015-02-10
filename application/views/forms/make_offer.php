67e8412b868acbacda942bbc46904b72d69e4ff3
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 18:30:00 -0600

    css

--- application/views/forms/make_offer.php
+++ application/views/forms/make_offer.php
@@ -1,13 +1,13 @@
 <div class="container">
 	
 	<div class="row">
-		<div class="col-sm-2 col-md-1">
+		<div class="col-xs-2 col-sm-1">
 			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
 		</div>
-		<div class="col-sm-10 col-md-11">
+		<div class="col-xs-10 col-sm-11">
 			<h1 class="">Make Offer: LISTING_TITLE_HERE</h1>
 		</div>
-		<div class="clearfix visible-sm-block"></div>
+		<div class="clearfix visible-xs-block"></div>
 	</div>
 	
 	
