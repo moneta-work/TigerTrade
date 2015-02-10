2de73d50024955c4654bdf0f01e0ce8043845b00
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 19:22:13 -0600

    css

--- application/views/forms/offer_response.php
+++ application/views/forms/offer_response.php
@@ -35,7 +35,7 @@
 				<div class="radio">
 					<label>
 						<input type="radio" id='accept-offer' name="decision" id="optionsRadios1" value="option1">
-						Accept Offer<p class="warning" id='offer-warning' style='display:none'> Accepting this offer will provide the buyer with your personal contact information.</p>
+						Accept Offer<p class="text-warning" id='offer-warning' style='display:none'> Buyer will receive your contact information.</p>
 					</label>
 				</div>
 				<div class="radio">
