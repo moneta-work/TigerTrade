9d3cad76b6934965b6bfec0d8e19ac76eb8bda88
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 19:17:54 -0600

    js

--- assets/js/show_hidden_message.js
+++ assets/js/show_hidden_message.js
@@ -1,7 +1,7 @@
 $('input[name=decision]').click(function () {
     if (this.id == "accept-offer") {
         $("#show-me").show('slow');
-        $("#show-me").display('inline');
+        $("#show-me").css('display', 'inline')
     } else {
         $("#show-me").hide('slow');
     }
