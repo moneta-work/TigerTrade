399bbc899c091780ddc4f987d7ff5ec0001d06cb
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 19:05:41 -0600

    js

--- application/views/shared/layout_main.php
+++ application/views/shared/layout_main.php
@@ -41,8 +41,8 @@
 		</div>
 	</footer>
 	
-	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 	<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 	<script src="<?php echo base_url('assets/js/back_button.js') ?>"></script>
 </body>
 </html>
