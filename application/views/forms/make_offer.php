2587eb14b208d54ffd74d1bf694041e2a3c243d1
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 18:36:26 -0600

    css

--- application/views/forms/make_offer.php
+++ application/views/forms/make_offer.php
@@ -10,23 +10,6 @@
 		</div>
 		<div class="clearfix visible-sm-block"></div>
 	</div>
-	
-	
-	
-	<div class="row">
-		<div class="col-xs-2 col-md-1">2 sm 1 md</div>
-		<div class="col-xs-10 col-md-11">10 sm 11 md</div>
-	</div>
-
-	<div class="row">
-		<div class="col-xs-2">2 sm</div>
-		<div class="col-xs-10">10 sm</div>
-	</div>
-	
-	<div class="row">
-		<div class="col-md-1">1 md</div>
-		<div class="col-md-11">11 md</div>
-	</div>
 
 	<form class="form-horizontal" id="make-offer-form">
 		<div class="form-group">
