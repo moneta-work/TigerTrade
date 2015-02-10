8b2bfc6c53da38da4f361e6aa12e37923cdbbbf9
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 18:14:41 -0600

    cs

--- application/views/ad/ad_view.php
+++ application/views/ad/ad_view.php
@@ -4,7 +4,7 @@
 			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
 		</div>
 		<div class="col-xs-10">
-			<h1 class="">Ad Index</h1>
+			<h1 class="back-button-header">Ad Index</h1>
 		</div>
 	</div>
 	
