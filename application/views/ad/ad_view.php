72e0846399462e9c914e506b19f18ad44cc00894
kylecarlson <krcz85@mail.missouri.edu>
2015-02-09 18:13:55 -0600

    css

--- application/views/ad/ad_view.php
+++ application/views/ad/ad_view.php
@@ -1,9 +1,9 @@
 <div class="container padding-top-20">
 	<div class="row">
-		<div class="col-lg-1 col-md-12">
+		<div class="col-xs-2">
 			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
 		</div>
-		<div class="col-lg-11 col-md-12">
+		<div class="col-xs-10">
 			<h1 class="">Ad Index</h1>
 		</div>
 	</div>
