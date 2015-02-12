38edd66c046968c67783b81007f2ceed75bb1972
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-12 17:09:48 -0600

    changed database hostname

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'kylecarlson.info';
 $db['default']['username'] = 'kylecarlson';
 $db['default']['password'] = 'QRvC3TMCBt';
 $db['default']['database'] = 'kylecarlson_tigertrade';
