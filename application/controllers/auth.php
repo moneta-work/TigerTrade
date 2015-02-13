7ea287c60b55fdfb6ac29babf6390f2c7505bb0b
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-12 19:08:06 -0600

    remove being admin to create user

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -419,11 +419,6 @@ class Auth extends CI_Controller {
 	{
 		$this->data['title'] = "Create User";
 
-		if (!$this->ion_auth->logged_in() || !$this->ion_auth->is_admin())
-		{
-			redirect('auth', 'refresh');
-		}
-
 		$tables = $this->config->item('tables','ion_auth');
 
 		//validate form input
