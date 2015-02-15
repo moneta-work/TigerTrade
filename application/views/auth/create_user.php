2420873dfd14362a7be167150bb298f735db6033
kylecarlson <krcz85@mail.missouri.edu>
2015-02-15 16:03:00 -0600

    fix

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,10 +1,10 @@
 <div class="container">
       <div id="infoMessage">
-	      <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-	      <div class="alert alert-danger" role="alert"></div>
-		      <span class="sr-only">Error:</span>
-		      <?php echo $message;?>
-		  
+		<div class="alert alert-danger" role="alert">
+		  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+		  <span class="sr-only">Error:</span>
+		  <?php echo $message;?>
+		</div>
 	  </div>
       <div class="row">
             <div class="col-xs-2 col-md-1">
