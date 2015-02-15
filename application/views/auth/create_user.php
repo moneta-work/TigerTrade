7943bba51f06e0da026cd5aecf10785de4f68842
kylecarlson <krcz85@mail.missouri.edu>
2015-02-15 16:03:27 -0600

    ewf

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,7 +1,6 @@
 <div class="container">
       <div id="infoMessage">
 		<div class="alert alert-danger" role="alert">
-		  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
 		  <span class="sr-only">Error:</span>
 		  <?php echo $message;?>
 		</div>
