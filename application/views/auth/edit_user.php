7f2c66d071a0d65f421dfe802795a7e50598409f
kylecarlson <krcz85@mail.missouri.edu>
2015-02-15 21:31:49 -0600

    html

--- application/views/auth/edit_user.php
+++ application/views/auth/edit_user.php
@@ -101,7 +101,7 @@
 					          'id'          => 'first_name',
 					          'class'       => 'form-control'
 					        );
-	                        echo form_input($first_name, $data);
+	                        echo form_input($data);
 	                    ?>
                   </div>
             </div>
--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -41,6 +41,5 @@
 		</tr>
 	<?php endforeach;?>
 </table>
-
 <p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?></p>
 </div>
