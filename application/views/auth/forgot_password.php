7b72e638bc54de24c25e547dba5afd3f3f535733
kylecarlson <krcz85@mail.missouri.edu>
2015-02-15 19:41:38 -0600

    html

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -1,3 +1,6 @@
+<!-- OLD FORGOT PASSWORD FORM -->
+
+<!--	
 <h1><?php echo lang('forgot_password_heading');?></h1>
 <p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
 
@@ -13,3 +16,62 @@
       <p><?php echo form_submit('submit', lang('forgot_password_submit_btn'));?></p>
 
 <?php echo form_close();?>
+-->
+
+<div class="container">
+      <div class="row">
+            <div class="col-xs-2 col-md-1">
+                  <div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+            </div>
+            <div class="col-xs-10 col-md-11">
+                  <h1 class="">Forgot Password</h1>
+                  <p>Enter your the email address you used to sign up.</p>
+                  <p>We will send you an email about resetting your password.</p>
+            </div>
+      </div>
+      <hr>
+      <?php if ($message != "") { ?>
+      <div id="infoMessage">
+		<div class="alert alert-info" role="alert" style="margin-top: 10px;">
+		  <span class="sr-only">Error:</span>
+		  <?php echo $message;?>
+		</div>
+	  </div>
+	  <?php }; ?>
+      
+
+      <?php echo form_open("auth/login", array('class' => 'form-horizontal', 'id' => 'ad-form'));?>
+            <div class="form-group">
+                  <label for="identity" class="col-sm-4 control-label label-20">Email</label>
+                  <div class="col-sm-4">
+                        <?php
+	                         $data = array(
+					          'name'        => 'identity',
+					          'id'          => 'identity',
+					          'class'       => 'form-control'
+					        );
+	                        echo form_input($data);
+	                    ?>
+                  </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-4">
+	                <div class="checkbox">
+					    <label style="padding-top: 7px;">
+                        <?php
+	                         $data = array(
+					          'name'        => 'remember',
+					          'id'          => 'remember',
+					          'class'       => ''
+					        );
+	                        echo form_checkbox($data);
+	                    ?>Remember me
+					    </label>
+					    <button type="submit" style="float: right;" class="btn btn-default">Login</button>
+	                </div>
+	                
+                </div>
+            </div>
+
+      <?php echo form_close();?>
+</div>
