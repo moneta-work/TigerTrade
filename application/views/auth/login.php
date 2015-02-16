6cb11e98ba8a355ef6397b242f615a33c058af31
kylecarlson <krcz85@mail.missouri.edu>
2015-02-15 19:14:19 -0600

    css

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -83,7 +83,7 @@
             <div class="form-group">
                 <div class="col-sm-offset-4 col-sm-4">
 	                <div class="checkbox">
-					    <label style="padding-top: 5px;">
+					    <label style="padding-top: 15px;">
                         <?php
 	                         $data = array(
 					          'name'        => 'remember',
