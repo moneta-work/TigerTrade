eaf78a5714bd1d4c27d8ec7d50a50f2d72ef1476
kylecarlson <krcz85@mail.missouri.edu>
2015-02-15 21:02:47 -0600

    html

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -20,7 +20,7 @@
             <td><?php echo htmlspecialchars($user->email,ENT_QUOTES,'UTF-8');?></td>
 			<td>
 				<?php foreach ($user->groups as $group):?>
-					<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?><br />
+					<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?>, 
                 <?php endforeach?>
 			</td>
 			<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></td>
@@ -30,3 +30,18 @@
 </table>
 
 <p><?php echo anchor('auth/create_user', lang('index_create_user_link'))?> | <?php echo anchor('auth/create_group', lang('index_create_group_link'))?></p>
+
+<div class="container padding-top-20">
+	<div class="row">
+		<div class="col-xs-2 col-md-1">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-10 col-md-11">
+			<h1 class="">User Homepage</h1>
+		</div>
+	</div>
+	
+	<hr>
+	
+	<p>This will be the homepage for logged in users.</p>
+</div>
