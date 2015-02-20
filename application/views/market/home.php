61998fed3a2bbc94499d10f187346ca394f0f43e
kylecarlson <krcz85@mail.missouri.edu>
2015-02-19 22:55:01 -0600

    html

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -12,18 +12,14 @@
 	
 	<div class="row">
 		<div class="col-xs-3">
-		<?php
-			foreach ($categories->result() as $category) { 
-			$count = 0;
-		?>
-		
+		<?php foreach ($categories->result() as $category) { $count = 0; ?>
 		    <a href="<?php echo base_url('/market/category/' . $category->category_id) ?>">
 			    <b><?php echo $category->name ?></b>
 			</a>
 			<br>
 	    	<?php foreach ($subcategories->result() as $subcategory) {
 	    		if ($subcategory->category_id == $category->category_id) { 
-		    		if ($count > 0) { ?> / <?php }
+		    		if ($count > 0) { ?><span class="hidden-xs"> / </span><?php }
 		    		$count++;
 	    		?>
 				    <a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id) ?>">
