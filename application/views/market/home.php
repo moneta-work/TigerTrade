864278e4d6749688c273d02d5617d1ff625b88ec
kylecarlson <krcz85@mail.missouri.edu>
2015-02-19 19:38:21 -0600

    text

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -1,7 +1,10 @@
 <div class="container padding-top-20">
 	<div class="row">
-		<div class="col-xs-12">
-			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div><h1 class="">Market Index</h1>
+		<div class="col-xs-2 col-md-1">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-10 col-md-11">
+			<h1 class="">Market Index</h1>
 		</div>
 	</div>
 	
