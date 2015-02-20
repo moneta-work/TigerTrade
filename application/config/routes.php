4da0b3c9788c402bbc6d19a7820b021f56d4bd4d
kylecarlson <krcz85@mail.missouri.edu>
2015-02-20 17:20:02 -0600

    route

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,7 +40,7 @@
 
 $route['default_controller'] = "home";
 $route['404_override'] = 'custom_404';
-$route['admin'] = 'admin/admin';
+$route['admin'] = 'admin/admin/index';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
