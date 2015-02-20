6f1d09a959be1b12f99a5424f8611d0b271c6ee1
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-19 19:54:52 -0600

    test for uploading form

--- application/controllers/ad.php
+++ application/controllers/ad.php
@@ -58,6 +58,87 @@ class Ad extends CI_Controller
 	//create an ad
 	function create()
 	{
+		$upload_conf = array(
+            'upload_path'   => realpath('assets/images'),
+            'allowed_types' => 'gif|jpg|png',
+            'max_size'      => '30000',
+            );
+    
+        $this->upload->initialize( $upload_conf );
+    
+        // Change $_FILES to new vars and loop them
+        foreach($_FILES['userfile'] as $key=>$val)
+        {
+            $i = 1;
+            foreach($val as $v)
+            {
+                $field_name = "file_".$i;
+                $_FILES[$field_name][$key] = $v;
+                $i++;   
+            }
+        }
+        // Unset the useless one ;)
+        unset($_FILES['userfile']);
+    
+        // Put each errors and upload data to an array
+        $error = array();
+        $success = array();
+        
+        // main action to upload each file
+        foreach($_FILES as $field_name => $file)
+        {
+            if ( ! $this->upload->do_upload($field_name))
+            {
+                // if upload fail, grab error 
+                $error['upload'][] = $this->upload->display_errors();
+            }
+            else
+            {
+                // otherwise, put the upload datas here.
+                // if you want to use database, put insert query in this loop
+                $upload_data = $this->upload->data();
+                
+                // set the resize config
+                $resize_conf = array(
+                    // it's something like "/full/path/to/the/image.jpg" maybe
+                    'source_image'  => $upload_data['full_path'], 
+                    // and it's "/full/path/to/the/" + "thumb_" + "image.jpg
+                    // or you can use 'create_thumbs' => true option instead
+                    'new_image'     => $upload_data['file_path'].'thumb_'.$upload_data['file_name'],
+                    'width'         => 200,
+                    'height'        => 200
+                    );
+
+                // initializing
+                $this->image_lib->initialize($resize_conf);
+
+                // do it!
+                if ( ! $this->image_lib->resize())
+                {
+                    // if got fail.
+                    $error['resize'][] = $this->image_lib->display_errors();
+                }
+                else
+                {
+                    // otherwise, put each upload data to an array.
+                    $success[] = $upload_data;
+                }
+            }
+        }
+
+        // see what we get
+        if(count($error > 0))
+        {
+            $data['error'] = true;
+        }
+        else
+        {
+            $data['created'] = true;
+        }
+        
+        $this->load->view('upload',$data);
+    }
+		/*
 		$config['upload_path'] = './assets/images/';
 			$config['allowed_types'] = 'gif|jpg|png|img|jpeg|pdf';
 			$config['max_size'] = '100';
@@ -82,7 +163,7 @@ class Ad extends CI_Controller
 			      	}
 			    }
 			}
-			
+		*/
 			$data['title'] = 'New Ad';
 			$this->layout->view('forms/new_ad', $data);
 		/*	
