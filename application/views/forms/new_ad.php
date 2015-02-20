f5f55c55987adc69431c1fdb607a408c3728daa0
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-19 19:59:10 -0600

    change name to user file

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -47,7 +47,7 @@
 		<div class="form-group" >
 			<label for="description" class="col-sm-2 control-label label-20">Upload Image</label>
 			<div class="col-sm-10">
-				<div id="filediv"><input name="file[]" type="file" id="file"/></div>
+				<div id="filediv"><input name="userfile[]" type="file" id="file"/></div>
 			</div>
 		</div>
 
