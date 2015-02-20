2ff478662621e7e8f617477531df288610299f0d
kylecarlson <krcz85@mail.missouri.edu>
2015-02-19 21:14:44 -0600

    php

--- application/controllers/market.php
+++ application/controllers/market.php
@@ -32,9 +32,16 @@ class Market extends CI_Controller
 	}
 
 	//shows details of a specific ad
-	function view_category($category_id)
+	function category($category_id)
 	{
 		$data['category'] = $this->category_model->get_category($category_id);
+		$data['title'] = ucfirst($category->name);
+		$this->layout->view('market/category_home', $data);
+	}
+	
+	function subcategory($category_id)
+	{
+		$data['subcategory'] = $this->subcategory_model->get_subcategory($category_id);
 		$data['title'] = 'A Category Home';
 		$this->layout->view('market/category_home', $data);
 	}
