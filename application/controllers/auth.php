aef4276188b86a6a64c71e93393114a88328a960
timvanhorn <timvanhorn@ymail.com>
2015-02-20 17:56:31 -0600

    update to controller

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -13,6 +13,12 @@ class Auth extends CI_Controller {
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
 		$this->lang->load('auth');
+		
+		if (!$this->ion_auth->logged_in())
+		{
+			//redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
 	}
 
 	//redirect if needed, otherwise display the user list
