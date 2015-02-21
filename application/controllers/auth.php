49965cd476463ce68777d2ddbd1ed51ef2d07adf
timvanhorn <timvanhorn@ymail.com>
2015-02-20 18:44:37 -0600

    test

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -529,8 +529,7 @@ class Auth extends CI_Controller {
 			// do we have a valid request?
 			if ($this->_valid_csrf_nonce() === FALSE || $id != $this->input->post('id'))
 			{
-				show_error($this->lang->line('error_csrf'));
-				echo "test";
+				//show_error($this->lang->line('error_csrf'));
 			}
 
 			//update the password if it was posted
