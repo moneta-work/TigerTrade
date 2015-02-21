046730f16a05819211208848f41e8e47184abbdc
timvanhorn <timvanhorn@ymail.com>
2015-02-21 12:28:20 -0600

    test

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -531,7 +531,7 @@ class Auth extends CI_Controller {
 			{
 				//show_error($this->lang->line('error_csrf'));
 				echo "test";
-				print $this->_valid_csrf_nonce();
+				print_r $this->_valid_csrf_nonce();
 				exit;
 				
 			}
