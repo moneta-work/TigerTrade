d567c19696ba3e36f0bad61a2b0925ff69701e8d
timvanhorn <timvanhorn@ymail.com>
2015-02-20 19:18:28 -0600

    test

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -530,7 +530,7 @@ class Auth extends CI_Controller {
 			if ($this->_valid_csrf_nonce() === FALSE || $id != $this->input->post('id'))
 			{
 				//show_error($this->lang->line('error_csrf'));
-				echo $this->input->post($this->session->flashdata('csrfkey'));
+				echo "<p>" . $this->input->post($this->session->flashdata('csrfkey')) . "</p>";
 				echo $this->session->flashdata('csrfvalue');
 				echo "hello";
 				exit;
