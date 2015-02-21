065cc7db9c573329677f65168f15dbcfdd221f6e
timvanhorn <timvanhorn@ymail.com>
2015-02-20 23:35:59 -0600

    changed config file

--- application/config/config.php
+++ application/config/config.php
@@ -244,8 +244,8 @@ $config['encryption_key'] = 'SessionKeyTigerTrade';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_cookie_name']		= 'cisession';
+$config['sess_expiration']		= 84200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
