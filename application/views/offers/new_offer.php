f6da6550cdb9842e6006835d7e06bc48291ae91f
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 05:15:17 -0600

    hmtl

--- application/views/offers/new_offer.php
+++ application/views/offers/new_offer.php
@@ -1,2 +1,22 @@
-<?php echo $error; ?>
-<?php echo $created; ?>
+<div class="container">
+
+	<div class="row">
+		<div class="col-xs-2 col-md-1">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-10 col-md-11">
+			<h1 class="">New Offer</h1>
+		</div>
+	</div>
+		
+	<?php if (isset($error)) { ?>
+		<p>Could not create your offer. Please make sure you entered a price and message.</p>
+	<?php } ?>
+	<?php if ($created) ?>
+		<h2>Offer Sent!</h2>
+		<p>buyer_id <?php echo $buyer_id; ?>, your offer for seller_id <?php echo $seller_id; ?>'s ad_id <?php echo $ad_id; ?> has been sent</p>
+		<p>You offered $<?php echo $price; ?>.</p>
+		<p>You said: <?php echo $buyer_message; ?>.</p>
+	<?php } ?>
+
+</div>
