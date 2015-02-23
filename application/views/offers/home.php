9c67e0c16b59a37c24ae0d25199ee967b613d526
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 04:11:01 -0600

    html

--- application/views/offers/home.php
+++ application/views/offers/home.php
@@ -13,13 +13,13 @@
 
 	<p>You can see your offer statuses here!</p>
 	<div class="row">
-		<a href="">
+		<a href="<?php echo base_url('/offers/pending') ?>">
 		<div class="col-sm-6">
 			<h2 class="text-center">Sent Offers</h2>
 			<img src="http://placehold.it/350x250" class="img-thumbnail" alt="Responsive image" style="width: 100%;">
 		</div>
 		</a>
-		<a href="">
+		<a href="<?php echo base_url('/offers/received') ?>">
 		<div class="col-sm-6">
 			<h2 class="text-center">Received Offers</h2>
 			<img src="http://placehold.it/350x250" class="img-thumbnail" alt="Responsive image" style="width: 100%;">
