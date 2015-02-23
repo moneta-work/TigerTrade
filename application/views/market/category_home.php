d89da1ba54f58c76f664a54da30690f1345c27b4
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 13:52:49 -0600

    html

--- application/views/market/category_home.php
+++ application/views/market/category_home.php
@@ -10,9 +10,39 @@
 	
 	<hr>
 	
-	<?php foreach ($subcategories->result() as $subcategory) { ?>
-		<a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id) ?>">
-			<?php echo $subcategory->name ?>
-		</a><br>
-	<?php } ?>
+	<div class="row">
+		<div class="col-xs-3">
+		<?php foreach ($subcategories->result() as $subcategory) { ?>
+			<a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id) ?>">
+				<?php echo $subcategory->name ?>
+			</a><br>
+		<?php } ?>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-xs-9">
+			<h2><?php echo ucfirst($category->name); ?> Ads</h2>
+			<table class="table">
+				<tr>
+					<th>Ad ID</th>
+					<th>Buyer ID</th>
+					<th>Seller ID</th>
+					<th>Buyer Message</th>
+					<th>Price</th>
+					<th>Status</th>
+				</tr>
+				<?php foreach ($pending->result() as $row) { ?>
+				<tr>
+					<td><?php echo $row->ad_id; ?></td>
+					<td><?php echo $row->buyer_id; ?></td>
+					<td><?php echo $row->seller_id; ?></td>
+					<td><?php echo $row->buyer_message; ?></td>
+					<td><?php echo $row->price; ?></td>
+					<td><?php echo $row->status; ?></td>
+				</tr>
+				<?php } ?>
+			</table>
+		</div>
+	</div>
 </div>
