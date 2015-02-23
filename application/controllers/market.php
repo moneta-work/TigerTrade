8feeed100a6db7ccbb054e0f66579699f77b3d72
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 14:25:13 -0600

    php

--- application/controllers/market.php
+++ application/controllers/market.php
@@ -42,9 +42,8 @@ class Market extends CI_Controller
 		$this->layout->view('market/category_home', $data);
 	}
 	
-	function subcategory($category_id, $subcategory_id)
+	function subcategory($subcategory_id)
 	{
-		$data['subcategories'] = $this->subcategory_model->get_subcategories($category_id);
 		$data['subcategory'] = $this->subcategory_model->get_subcategory($subcategory_id);
 		$data['ads'] = $this->ad_model->get_ads_subcategory($subcategory_id);
 		$data['title'] = 'Subcategory Home';
