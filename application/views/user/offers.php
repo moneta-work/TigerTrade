997a52e5f51abe359ae0e32898206c3b1db78995
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 03:59:00 -0600

    html

---
+++ application/views/user/offers.php
@@ -0,0 +1,24 @@
+<div class="container">
+
+	<div class="row">
+		<div class="col-xs-2 col-md-1">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-10 col-md-11">
+			<h1 class="">Offers</h1>
+		</div>
+	</div>
+
+	<hr>
+
+	<p>You can see your offer statuses here!</p>
+	<div class="row">
+		<div class="col-sm-6">
+			<h2>Sent Offers</h2>
+		</div>
+		<div class="col-sm-6">
+			<h2>Received Offers</h2>
+		</div>
+	</div>
+	
+</div>
