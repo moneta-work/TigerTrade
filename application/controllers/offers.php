76727c194e39c00af92e3a2d0847937d0b3aaef4
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 05:33:06 -0600

    php

--- application/controllers/offers.php
+++ application/controllers/offers.php
@@ -37,6 +37,7 @@ class Offers extends CI_Controller
 		//if validation passes
 		else
 		{
+			$ad_id = $ad_id;
 			$seller_id = 1;
 			$buyer_id = $user->id;
 			$price = $this->security->xss_clean($this->input->post('price'));
