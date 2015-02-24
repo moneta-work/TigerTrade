afbc4ce7d4fad7dc9ffb42598674ab3a74796ce7
timvanhorn <timvanhorn@ymail.com>
2015-02-24 16:37:55 -0600

    form

--- application/views/forms/new_category.php
+++ application/views/forms/new_category.php
@@ -15,6 +15,19 @@
 	<?php echo form_open("market/new_category", array('class' => 'form-horizontal', 'id' => 'ad-form', 'enctype' => 'multipart/form-data'));?>	
 		
 		<div class="form-group">
+			<label for="list" class="col-sm-2 control-label label-20">Categories</label>
+			<div class="col-sm-10">
+				<select size="3" name="list">
+				<option selected>Plan 1
+				<option>Plan 2
+				<option>Plan 3
+				<option>Plan 4
+				<option>Plan 5
+				</select>
+			</div>
+		</div>
+		
+		<div class="form-group">
 			<label for="category_name" class="col-sm-2 control-label label-20">Category Name</label>
 			<div class="col-sm-10">
 				<input type="text" class="form-control" name="category_name" id="category_name" placeholder="">
