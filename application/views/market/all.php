0b5d1409693cb47c0c5498e57df9ca1c6fc43844
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 21:10:32 -0600

    php

--- application/views/market/all.php
+++ application/views/market/all.php
@@ -55,8 +55,8 @@
 				<?php foreach ($ads->result() as $row) { ?>
 				<div class="col-sm-6 col-md-4">
 					<h3><?php echo $row->title; ?></h3>
-					<img src="http://placehold.it/300x200" class="img-thumbnail" alt="Responsive image" style="width: 100%;">
 					<p>Price: $<?php echo $row->price; ?></p>
+					<img src="http://placehold.it/300x200" class="img-thumbnail" alt="Responsive image" style="width: 100%;"><br>
 					<p>Description: <?php echo $row->description; ?></p>
 					<p>Ad ID: <?php echo $row->ad_id; ?></p>
 				</div>
