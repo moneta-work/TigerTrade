176c6b5b146eb775d86baeb64b2f01f2bf92d8a2
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 20:53:54 -0600

    php

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -16,19 +16,11 @@
 			    <b>all</b>
 			</a>
 			<br><br>
-			<?php foreach ($categories->result() as $category) { $count = 0; ?>
-			    <a href="<?php echo base_url('/market/category/' . $category->category_id) ?>">
-				    <b><?php echo $category->name ?></b>
-				</a>
-				<br>
+			<?php foreach ($categories->result() as $category) { ?>
+			    <a href="<?php echo base_url('/market/category/' . $category->category_id); ?>"><b><?php echo $category->name; ?></b></a><br>
 		    	<?php foreach ($subcategories->result() as $subcategory) {
-		    		if ($subcategory->category_id == $category->category_id) { 
-			    		if ($count > 0) { ?><span class="hidden-xs"> / </span><?php }
-			    		$count++;
-		    		?>
-					    <a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id) ?>">
-						    <?php echo $subcategory->name ?>
-						</a>
+		    		if ($subcategory->category_id == $category->category_id) { ?>
+					    <a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id); ?>"><?php echo $subcategory->name; ?></a><br>
 					<?php } ?>
 		    	<?php } ?>
 		    	<br><br>
