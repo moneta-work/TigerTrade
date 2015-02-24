5786b4b80d2a55e82e9ed93a8ec27490ba5c6c50
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 22:15:36 -0600

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -27,6 +27,13 @@
 		<div class="col-xs-9 col-sm-10">
 			<div class="row">
 				<div class="alert alert-info row">
+					<div class="btn-group" role="group" aria-label="...">
+						<button type="button" class="btn btn-default" href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</button>
+						<button type="button" class="btn btn-default">Make an Offer</button>
+						<button type="button" class="btn btn-default">Review an Offer</button>
+						<button type="button" class="btn btn-default">Create a Category</button>
+						<button type="button" class="btn btn-default">Create a Subcategory</button>
+					</div>
 					<p>Form for new ad: <a href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a></p>
 					<p>Form for new offer: <a href="<?php echo base_url('/ad/make_offer') ?>">Make an Offer</a></p>
 					<p>Form to respond to an offer: <a href="<?php echo base_url('/ad/review_offer') ?>">Review an Offer</a></p>
