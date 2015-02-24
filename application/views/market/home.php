ce5c40a2ab58febd77b9e759844ad4946ce6ab1b
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 20:55:48 -0600

    php

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -12,7 +12,7 @@
 	
 	<div class="row">
 		<div class="col-xs-3">
-			<a href="<?php echo base_url('/market/all') ?>"><b>all</b></a><br>
+			<a href="<?php echo base_url('/market/all') ?>"><b>all</b></a><br><br>
 			<?php foreach ($categories->result() as $category) { ?>
 			    <a href="<?php echo base_url('/market/category/' . $category->category_id); ?>"><b><?php echo $category->name; ?></b></a><br>
 		    	<?php foreach ($subcategories->result() as $subcategory) {
