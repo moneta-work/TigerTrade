2a44499e343f2d3185611ab33449d0b17cc7c12b
kylecarlson <krcz85@mail.missouri.edu>
2015-02-24 03:14:12 -0600

    response page

---
+++ application/views/offers/response.php
@@ -0,0 +1,15 @@
+<div class="container padding-top-20">
+	<div class="row">
+		<div class="col-xs-3 col-sm-2 text-center">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-9 col-sm-10">
+			<h1>Offer Response</h1>
+		</div>
+	</div>
+
+	<hr>
+
+	<?php var_dump($_POST); ?>
+
+</div>
