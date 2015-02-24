aa702a9352b0be48bdcbba146bd204a7c3b0dc8e
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 21:22:02 -0600

    css

--- application/views/market/all.php
+++ application/views/market/all.php
@@ -54,7 +54,7 @@
 			<div class="row">
 				<?php foreach ($ads->result() as $row) { ?>
 				<a href="<?php echo base_url('/ad/' . $row->ad_id) ?>">
-					<div class="col-sm-6 col-md-4" style="padding-bottom: 10px;">
+					<div class="col-sm-6 col-md-4 market-ad" style="padding-bottom: 10px;">
 						<h3><?php echo $row->title; ?></h3>
 						<p>Price: $<?php echo $row->price; ?></p>
 						<img src="http://placehold.it/300x200" class="img-thumbnail" alt="Responsive image" style="width: 100%;"><br><br>
