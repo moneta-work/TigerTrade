1fb6c1725cb5d20ded96daa6d702469ba892977a
kylecarlson <krcz85@mail.missouri.edu>
2015-02-24 03:39:03 -0600

    css

--- application/views/offers/response.php
+++ application/views/offers/response.php
@@ -11,5 +11,15 @@
 	<hr>
 
 	<?php var_dump($_POST); ?>
-
+	
+	<?php if ($status == 'Accepted') { ?>
+		<h2>Offer Accepted!</h2>
+		<p>You will receive an email with your buyer's contact information.</p>
+	<?php } else if ($status == 'Declined') { ?>
+		<h2>Offer Declined!</h2>
+		<p>You'll get a better one, anyway!</p>
+	<?php } else { ?>
+		<h2>Something went wrong!</h2>
+		<p>The status was not set to 'Accepted' or 'Declined'...!</p>
+	<?php } ?>
 </div>
