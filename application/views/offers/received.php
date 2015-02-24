90ef9eb499f72c96fc2989805606e2f0a36b9826
kylecarlson <krcz85@mail.missouri.edu>
2015-02-24 00:22:08 -0600

    css

--- application/views/offers/received.php
+++ application/views/offers/received.php
@@ -25,14 +25,14 @@
 			<th>Status</th>
 		</tr>
 		<?php foreach ($pending->result() as $row) { ?>
-		<tr class='clickable-row' data-href='<?php echo base_url('/offers/review_offer/' . $row->offer_id) ?>'>
+		<a class="btn btn-default" href="<?php echo base_url('/offers/review_offer/' . $row->offer_id) ?>"><tr>
 			<td><?php echo $row->ad_id; ?></td>
 			<td><?php echo $row->buyer_id; ?></td>
 			<td><?php echo $row->seller_id; ?></td>
 			<td><?php echo $row->buyer_message; ?></td>
 			<td><?php echo $row->price; ?></td>
 			<td><?php echo $row->status; ?></td>
-		</tr>
+		</tr></a>
 		<?php } ?>
 	</table>
 
