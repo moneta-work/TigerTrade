9389ece653884167d2330a609349da06e381424c
kylecarlson <krcz85@mail.missouri.edu>
2015-02-23 22:12:51 -0600

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -26,9 +26,7 @@
 		
 		<div class="col-xs-9 col-sm-10">
 			<div class="row">
-				<div class="row">
-					<p>Main page for categories.</p>
-					<br>
+				<div class="alert alert-danger row">
 					<p>Form for new ad: <a href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a></p>
 					<p>Form for new offer: <a href="<?php echo base_url('/ad/make_offer') ?>">Make an Offer</a></p>
 					<p>Form to respond to an offer: <a href="<?php echo base_url('/ad/review_offer') ?>">Review an Offer</a></p>
