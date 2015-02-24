03e7157462a219ea64bda2bfb6952b2e17e986b5
kylecarlson <krcz85@mail.missouri.edu>
2015-02-24 03:31:01 -0600

    css

--- application/views/offers/details.php
+++ application/views/offers/details.php
@@ -72,7 +72,7 @@
 		</div>
 		<div class="form-group">
 			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-default">Submit</button>
+				<button type="submit" class="btn btn-default" disabled>Submit</button>
 			</div>
 		</div>
 	<?php echo form_close();?>
