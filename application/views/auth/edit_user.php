5e33110af2869f7c1292cac16f6d8aba6e481609
timvanhorn <timvanhorn@ymail.com>
2015-02-24 20:02:21 -0600

    test

--- application/views/auth/edit_user.php
+++ application/views/auth/edit_user.php
@@ -76,6 +76,30 @@
                   </div>
             </div>
 			
+			<?php if ($this->ion_auth->is_admin()): ?>
+
+          <h3><?php echo lang('edit_user_groups_heading');?></h3>
+          <?php foreach ($groups as $group):?>
+              <label class="checkbox">
+              <?php
+                  $gID=$group['id'];
+                  $checked = null;
+                  $item = null;
+                  foreach($currentGroups as $grp) {
+                      if ($gID == $grp->id) {
+                          $checked= ' checked="checked"';
+                      break;
+                      }
+                  }
+              ?>
+              <input type="checkbox" name="groups[]" value="<?php echo $group['id'];?>"<?php echo $checked;?>>
+              <?php echo htmlspecialchars($group['name'],ENT_QUOTES,'UTF-8');?>
+              </label>
+			<?php endforeach?>
+
+			<?php endif ?>
+			
+			
 			<?php 
 			echo form_hidden('id', $user->id);
 			echo form_hidden($csrf); 
