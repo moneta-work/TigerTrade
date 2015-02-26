e54134ecd231f30726fcac074e10a162e364a38c
timvanhorn <timvanhorn@ymail.com>
2015-02-26 14:42:17 -0600

    test

--- application/views/auth/deactivate_user.php
+++ application/views/auth/deactivate_user.php
@@ -1,7 +1,31 @@
-<h1><?php echo lang('deactivate_heading');?></h1>
-<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
 
-<?php echo form_open("auth/deactivate/".$user->id);?>
+<div class="container padding-top-20">
+
+	<div class="row">
+		<div class="col-xs-3 col-sm-2 text-center">
+			<div class="back-button"><button class="btn btn-default" onclick="goBack()">Back</button></div>
+		</div>
+		<div class="col-xs-9 col-sm-10">
+			<h1>User Activation</h1>
+		</div>
+	</div>
+	
+      <hr>
+      <?php if ($message != "") { ?>
+      <div id="infoMessage">
+		<div class="alert alert-info" role="alert" style="margin-top: 10px;">
+		  <span class="sr-only">Error:</span>
+		  <?php echo $message;?>
+		</div>
+	  </div>
+	  <?php }; ?>
+
+
+	<h1><?php echo lang('deactivate_heading');?></h1>
+	<p><?php echo sprintf(lang('deactivate_subheading'), $user->username);?></p>
+
+	<?php echo form_open(uri_string(), array('class' => 'form-horizontal', 'id' => 'ad-form'));?>
+	<?php// echo form_open("auth/deactivate/".$user->id);?>
 
   <p>
   	<?php echo lang('deactivate_confirm_y_label', 'confirm');?>
@@ -9,12 +33,12 @@
     <?php echo lang('deactivate_confirm_n_label', 'confirm');?>
     <input type="radio" name="confirm" value="no" />
   </p>
-  
-  <p>testing 123</p>
 
   <?php echo form_hidden($csrf); ?>
   <?php echo form_hidden(array('id'=>$user->id)); ?>
 
-  <p><?php echo form_submit('submit', lang('deactivate_submit_btn'));?></p>
+	<p><?php echo form_submit('submit', lang('deactivate_submit_btn'));?></p>
 
-<?php echo form_close();?>
+	<?php echo form_close();?>
+
+</div>
