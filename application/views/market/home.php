4f2c3e34ac2f1aa13b334ba5daf9fa7379647637
kylecarlson <krcz85@mail.missouri.edu>
2015-02-25 23:37:59 -0600

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -16,10 +16,10 @@
 	<div class="row">
 		<div class="col-xs-1"><a href="<?php echo base_url('/market') ?>"><b>all</b></a></div><br>
 		<?php foreach ($categories->result() as $category) { ?>
-		    <div class="col-xs-1"><a href="<?php echo base_url('/market/category/' . $category->category_id); ?>"><b><?php echo $category->name; ?></b></a></div>
+		    <div class="row"><div class="col-xs-1"><a href="<?php echo base_url('/market/category/' . $category->category_id); ?>"><b><?php echo $category->name; ?></b></a></div>
 	    	<?php foreach ($subcategories->result() as $subcategory) {
 	    		if ($subcategory->category_id == $category->category_id) { ?>
-				    <div class="col-xs-1"><a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id); ?>"><?php echo $subcategory->name; ?></a></div>
+				    <div class="col-xs-1"><a href="<?php echo base_url('/market/subcategory/' . $subcategory->subcategory_id); ?>"><?php echo $subcategory->name; ?></a></div></div>
 				<?php } ?>
 	    	<?php } ?>
 	    	<br>
