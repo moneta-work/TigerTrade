0bce93877c99301013a3850d0fd408f9182c667d
kylecarlson <krcz85@mail.missouri.edu>
2015-02-25 22:35:17 -0600

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -26,7 +26,7 @@
 		
 		<div class="col-xs-9 col-sm-10">
 			<div class="row">
-				<div class="form-group col-sm-4">
+				<div class="form-group col-sm-2">
 					<label for="list" class="control-label label-20">Categories</label>
 					<select multiple size="10" class="form-control btn btn-mini" id="list" >
 					<?php 
@@ -35,8 +35,9 @@
 						endforeach; 
 					?>
 					</select>
+					<span class="caret"></span>
 				</div>
-				<div class="form-group col-sm-4">
+				<div class="form-group col-sm-2">
 					<label for="list" class="control-label label-20">Categories</label>
 					<select multiple size="10" class="form-control btn btn-mini" id="list" >
 					<?php 
@@ -45,6 +46,7 @@
 						endforeach; 
 					?>
 					</select>
+					<span class="caret"></span>
 				</div>				
 			</div>
 			<div class="row">
