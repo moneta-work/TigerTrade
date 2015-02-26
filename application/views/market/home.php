3d056aacfcc8c50ae30a384843a5f587deb5e85b
kylecarlson <krcz85@mail.missouri.edu>
2015-02-26 00:55:14 -0600

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -11,11 +11,11 @@ $('#category').change(function(){
               {
                 $('#subcategory').empty();
                 $('#subcategory, #subcategory_label').show();
-                   $.each(subcategories,function(subcategory_id,name) 
+                   $.each(subcategories,function(subcategory_id,subcategory) 
                    {
                     var opt = $('<option />'); // here we're creating a new select option for each group
                       opt.val(subcategory_id);
-                      opt.text(name);
+                      opt.text(subcategory);
                       $('#subcategory').append(opt); 
                 });
                } //end success
