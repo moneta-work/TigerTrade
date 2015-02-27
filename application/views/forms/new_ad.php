e07a1bf3574a89d124716584f9c143e4f9012827
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 18:32:42 -0600

    js

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -1,33 +1,4 @@
-<script type="text/javascript">
-$(document).ready(function () {
-	alert("ready!");
-	/*
-	$('#categorySelectForm').change(function () {
-	  	var value = $(this).val();
-	  	console.log(value);
-	    
-	    if(value != "")
-	    {
-	    	$.ajax({
-                        url: "<?php echo base_url('ad');?>/get_subcategories/"+value,
-                        type: 'post',
-                        dataType: 'json',
-                        data: data,
-                     success: function (data) {
-                            if (data != null) {
-                                console.log(data);
-                            }
-                        }
-                    });
 
-	    }
-	    
-		
-	});	
-*/
-
-});
-</script>
 
 <div class="container padding-top-20">
 	<div class="row">
--- assets/js/new_ad.js
+++ assets/js/new_ad.js
@@ -1,1 +1,28 @@
+$(document).ready(function () {
+	alert("ready!");
+	
+	$('#categorySelectForm').change(function () {
+	  	var value = $(this).val();
+	  	console.log(value);
+	    
+	    if(value != "")
+	    {
+	    	$.ajax({
+                        url: "<?php echo base_url('ad');?>/get_subcategories/"+value,
+                        type: 'post',
+                        dataType: 'json',
+                        data: data,
+                     success: function (data) {
+                            if (data != null) {
+                                console.log(data);
+                            }
+                        }
+                    });
 
+	    }
+	    
+		
+	});	
+
+
+});
