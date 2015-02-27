4715e4a997193dc8e164a221f9d1e5ee7205f4bd
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 19:42:21 -0600

    js

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -10,7 +10,6 @@ class Ajax extends CI_Controller
 
 	function get_subcategories($category_id)
 	{
-		//header('Content-Type: application/x-json; charset=utf-8');
         echo(json_encode($this->subcategory_model->get_subcategories($category_id)));
 	}
 
