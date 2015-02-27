761b3a1f5f6531d165d34ce1fcd52fe9327607fc
kylecarlson <krcz85@mail.missouri.edu>
2015-02-26 18:46:02 -0600

    css

--- application/views/market/category_home.php
+++ application/views/market/category_home.php
@@ -14,7 +14,7 @@
 		<div class="col-xs-3 col-sm-2">
 			<!-- Market Menu -->
 			<div id="market-menu" class="text-center">
-				<a class="btn btn-primary btn-sm wide-button" role="button" href="<?php echo base_url('/market') ?>"><b>all</b></a><br><br>
+				<a class="btn btn-default btn-sm wide-button" role="button" href="<?php echo base_url('/market') ?>"><b>all</b></a><br><br>
 				<?php foreach ($categories->result() as $category) { ?>
 				    <a class="btn btn-default btn-sm wide-button" role="button" href="<?php echo base_url('/market/category/' . $category->category_id); ?>"><b><?php echo $category->name; ?></b></a><br>
 				    <select onchange="location = this.options[this.selectedIndex].value;" class="form-control" id="" >
