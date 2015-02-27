a96ee071c4e393d7a1809dfe8029e9301198dc77
kylecarlson <krcz85@mail.missouri.edu>
2015-02-26 19:14:57 -0600

    css

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -74,7 +74,8 @@
 			<div class="row">
 				
 				<!-- Buttons on top of page -->
-				<div class="row text-center visible-md">
+				<div class="hidden-xs">
+				<div class="row text-center">
 					<div class="btn-group hidden-xs">
 						<a class="btn btn-default btn-sm" href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a>
 						<?php if ($this->ion_auth->is_admin()) { ?>
@@ -89,7 +90,8 @@
 						<a class="btn btn-default btn-sm" href="<?php echo base_url('/ad/new_subcategory') ?>">Create a Subcategory</a><br>
 						<?php } ?>
 					</div>
-				</div>				
+				</div>
+				</div>
 								
 				<?php $count = 0; ?>
 				<?php foreach ($ads->result() as $row) { ?>
