099780c1ece5892bb9a17a801f05923b6926b3bd
kylecarlson <krcz85@mail.missouri.edu>
2015-02-26 18:27:20 -0600

    js

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -63,7 +63,7 @@
 				<a href="<?php echo base_url('/market') ?>"><b>all</b></a><br><br>
 				<?php foreach ($categories->result() as $category) { ?>
 				    <a href="<?php echo base_url('/market/category/' . $category->category_id); ?>"><b><?php echo $category->name; ?></b></a>
-				    <select class="form-control" id="" >
+				    <select onchange="location = this.options[this.selectedIndex].value;" class="form-control" id="" >
 					    	<option>Subcategory</option>
 			    	<?php foreach ($subcategories->result() as $subcategory) { ?>
 				    	
