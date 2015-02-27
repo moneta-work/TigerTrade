33c1b52d5bc0034a465e4a754a99bf8cea8529b1
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 18:37:43 -0600

    js

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -1,4 +1,33 @@
+<script type="text/javascript">
+	$(document).ready(function () {
+	alert("ready!");
+	
+	$('#categorySelectForm').change(function () {
+	  	var value = $(this).val();
+	    
+	    if(value != "")
+	    {
+	    	$.ajax({
+                        url: "<?php echo base_url('ad');?>/get_subcategories/"+value,
+                        type: 'post',
+                        dataType: 'json',
+                     success: function (data) {
+                        	console.log("Success");
+                            console.log(data);
+                        },
+                        failure: function (data){
+                        	console.log("failure")
+                        }
+                    });
 
+	    }
+	    
+		
+	});	
+
+
+});
+</script>
 
 <div class="container padding-top-20">
 	<div class="row">
