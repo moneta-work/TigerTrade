a90eb2f503d23203b242b3d5a3261648d5fd4010
timvanhorn <timvanhorn@ymail.com>
2015-02-26 18:01:13 -0600

    format test

--- application/views/auth/edit_group.php
+++ application/views/auth/edit_group.php
@@ -16,7 +16,10 @@
 <div id="infoMessage"><?php echo $message;?></div>
 
 <h3 class="col-sm-offset-3 col-sm-10">Please enter the group information below</h3>
-<br><br>
+
+<div class="row">
+<br>
+</div>
 
 <?php echo form_open(current_url(), array('class' => 'form-horizontal'));?>
 		
