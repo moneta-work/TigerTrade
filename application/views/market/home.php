1b6947e5477cc7d542f7a87682c989dd104b7cc0
timvanhorn <timvanhorn@ymail.com>
2015-02-26 18:48:30 -0600

    update market home view

--- application/views/market/home.php
+++ application/views/market/home.php
@@ -105,14 +105,14 @@
 						<a class="btn btn-default" href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a>
 						<?php if ($this->ion_auth->is_admin()) { ?>
 						<a class="btn btn-default" href="<?php echo base_url('/market/new_category') ?>">Create a Category</a>
-						<a class="btn btn-default" href="<?php echo base_url('/ad/new_subcategory') ?>">Create a Subcategory</a>
+						<a class="btn btn-default" href="<?php echo base_url('/market/new_subcategory') ?>">Create a Subcategory</a>
 						<?php } ?>
 					</div>
 					<div class="visible-xs">
 						<a class="btn btn-default" href="<?php echo base_url('/ad/new_ad') ?>">Place an Ad</a><br><br>
 						<?php if ($this->ion_auth->is_admin()) { ?>
 						<a class="btn btn-default" href="<?php echo base_url('/market/new_category') ?>">Create a Category</a><br><br>
-						<a class="btn btn-default" href="<?php echo base_url('/ad/new_subcategory') ?>">Create a Subcategory</a><br>
+						<a class="btn btn-default" href="<?php echo base_url('/market/new_subcategory') ?>">Create a Subcategory</a><br>
 						<?php } ?>
 					</div>
 				</div>
