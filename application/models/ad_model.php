2e7e42354e56012326c9cb62f0f7ac77a30b0da4
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 19:51:03 -0600

    php

--- application/models/ad_model.php
+++ application/models/ad_model.php
@@ -39,7 +39,7 @@ class Ad_model extends CI_Model
 
         if($query->result()){
             foreach ($query->result() as $sub) {
-                $subcategories[$subcategory->id] = $sub->name;
+                $subcategories[$sub->id] = $sub->name;
             }
             return $subcategories;
         } else {
