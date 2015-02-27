c0dae5af0f10603dc8039469c618afe022e20871
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 20:06:52 -0600

    js

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -1,7 +1,34 @@
 <script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
 
 <script type="text/javascript">
-
+$(document).ready(function (){
+	$('#categorySelectForm').change(function(){
+	    var category_id = $(this).val();
+	    if (category_id != ""){
+	        var post_url = "<?php echo base_url('ajax') ?>/get_subcategories/" + category_id;
+	        $.ajax({
+	            type: "POST",
+	            url: post_url,
+	            success: function(subCategories) //we're calling the response json array 'cities'
+	            {
+	                $('#subCategory').empty();
+	                subCategories = $.parseJSON(subCategories);
+                   	$.each(subCategories,function(id,name) 
+                   	{	
+                    	var opt = $('<option />'); // here we're creating a new select option for each group
+                      	opt.val(id);
+                      	opt.text(name);
+                      	$('#subCategory').append(opt); 	
+	                });
+	                console.log(subCategories);
+	        		
+	            } //end success
+	         }); //end AJAX
+	    } else {
+	        $('#subCategoryForm').hide();
+	    }//end if
+	}); //end change 
+});
 
 </script>
 
