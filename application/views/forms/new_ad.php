affbee83b6aa8b0445d96ee78d049548bb2b134c
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 19:25:13 -0600

    js

--- application/views/forms/new_ad.php
+++ application/views/forms/new_ad.php
@@ -1,4 +1,38 @@
+<script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
 
+<script type="text/javascript">
+	$(document).ready(function (){
+	$('#categorySelectForm').change(function(){
+    
+    var value = $(this).val();
+    alert(value);
+    /*if (state_id != ""){
+        var post_url = "/index.php/control_form/get_cities/" + state_id;
+        $.ajax({
+            type: "POST",
+             url: post_url,
+             success: function(cities) //we're calling the response json array 'cities'
+              {
+                $('#f_city').empty();
+                $('#f_city, #f_city_label').show();
+                   $.each(cities,function(id,city) 
+                   {
+                    var opt = $('<option />'); // here we're creating a new select option for each group
+                      opt.val(id);
+                      opt.text(city);
+                      $('#f_city').append(opt); 
+                });
+               } //end success
+         }); //end AJAX
+    } else {
+        $('#f_city').empty();
+        $('#f_city, #f_city_label').hide();
+    }//end if
+    */
+}); //end change 
+});
+
+</script>
 
 <div class="container padding-top-20">
 	<div class="row">
