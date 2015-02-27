aad332fc048a696be51166326a4de60b2de89e27
Tim Gilman <timothy.gilman@yahoo.com>
2015-02-26 19:40:43 -0600

    php test

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -5,17 +5,18 @@ class Ajax extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->library('upload');
-        $this->load->library('image_lib');
-		$this->load->model('ad_model');
 		$this->load->model('subcategory_model');
-		$this->load->model('category_model');
 	}
 
 	function get_subcategories($category_id)
 	{
 		//header('Content-Type: application/x-json; charset=utf-8');
         echo(json_encode($this->subcategory_model->get_subcategories($category_id)));
+	}
+
+	function test()
+	{
+		echo "hello";
 	}
 
 }
