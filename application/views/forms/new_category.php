a601517a68bae032a60698f69bb7db0d5db6de0c
timvanhorn <timvanhorn@ymail.com>
2015-03-01 15:22:34 -0600

    test

--- application/views/forms/new_category.php
+++ application/views/forms/new_category.php
@@ -13,20 +13,10 @@
 
 	<?php echo form_open("market/new_category", array('class' => 'form-horizontal', 'id' => 'ad-form', 'enctype' => 'multipart/form-data'));?>	
 		
-		<?php 
-			$count = sizeof($categories->result());
-			echo $count;
-			/*
-			foreach($categories->result() as $category):
-			$count++;
-			endforeach;
-				*/
-		?>
-		
 		<div class="form-group">
 			<label for="list" class="col-sm-2 control-label label-20">Categories</label>
 			<div class="col-sm-10">
-				<select size="<?php echo $count; ?>" class="form-control" id="list" >
+				<select size="<?php echo sizeof($categories->result()); ?>" class="form-control" id="list" >
 				<?php 
 					foreach($categories->result() as $category):
 					echo "<option disabled>" . $category->name . "</option>";
