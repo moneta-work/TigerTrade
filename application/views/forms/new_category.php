c80ed7203e509d893208d4143bd410155e17a0b1
timvanhorn <timvanhorn@ymail.com>
2015-03-01 15:25:06 -0600

    test

--- application/views/forms/new_category.php
+++ application/views/forms/new_category.php
@@ -15,7 +15,7 @@
 		
 		<div class="form-group">
 			<label for="list" class="col-sm-2 control-label label-20">Categories</label>
-			<div class="col-sm-10">
+			<div class="col-sm-6">
 				<select size="<?php echo sizeof($categories->result()); ?>" class="form-control" id="list" >
 				<?php 
 					foreach($categories->result() as $category):
@@ -30,20 +30,20 @@
 		
 		<div class="form-group">
 			<label for="category_name" class="col-sm-2 control-label label-20">Category Name</label>
-			<div class="col-sm-10">
+			<div class="col-sm-6">
 				<input type="text" class="form-control" name="category_name" id="category_name" required>
 			</div>
 		</div>
 		
 		<div class="form-group">
 			<label for="category_description" class="col-sm-2 control-label label-20">Description</label>
-			<div class="col-sm-10">
+			<div class="col-sm-6">
 				<input type="text" class="form-control" name="category_description" id="category_description" placeholder="">
 			</div>
 		</div>
 		
 		<div class="form-group">
-			<div class="col-sm-offset-2 col-sm-10">
+			<div class="col-sm-offset-2 col-sm-6">
 				<button type="submit" class="btn btn-default">Add</button>
 			</div>
 		</div>
